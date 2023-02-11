19eb96485454201574786b7ad16c7bb54f9d6769
Juan De La Cruz Vílchez Mora <vilchezdefault08@gmail.com>
2023-02-11 10:58:06 -0600

    vilchezdefault - 1/022023 Intefez implementation

---
+++ PETS_SOS/Forms/Owner.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PETS_SOS.Forms
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Owner : Window
+    {
+        public Owner()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show(" Número Identificación: " + txtPhoneNumber + "\n" +
+                                " Primer Nombre: " + txtFirstName + "\n" +
+                                " Segundo Nombre: " + txtsecondName.Text + "\n" +
+                                " Primer Apellido: " + txtLastName.Text + "\n" +
+                                " Segundo Apellido: " + txtSecondLastName.Text + "\n" +
+                                " Correo Electrónico: " + txtEmail.Text + "\n" +
+                                " Teléfono Celular: " + txtPhoneNumber.Text + "\n");
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                MessageBox.Show(error);
+            }
+        }
+
+        private void BtnIndex(object sender, RoutedEventArgs e)
+        {
+            var ventana = new Pet();
+
+            ventana.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PETS_SOS/Forms/Pet.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PETS_SOS.Forms
+{
+    /// <summary>
+    /// Interaction logic for Pet.xaml
+    /// </summary>
+    public partial class Pet : Window
+    {
+        public Pet()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ventana = new MainWindow();
+
+            ventana.Show();
+        }
+    }
+}
